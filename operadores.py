88e9387416beca43a615e329d14dfedab236eb2e
nunesWhay <gustavo.nunes200247@gmail.com>
2024-06-01 11:56:39 -0300

    tipos de operadores

---
+++ operadores.py
@@ -0,0 +1,9 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 ** produto_2)
